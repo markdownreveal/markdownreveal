66670d08a6a240b3bfae5f8a8ff23b7f24743983
Miguel Sánchez de León Peque <peque@neosit.es>
2017-03-11 00:23:15 +0100

    Remove year from copyright notice

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Markdownreveal'
-copyright = u'2016, The Markdownreveal contributors.'
+copyright = u'The Markdownreveal contributors.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
